b0fa40a852212e78734dc0a4686c4cca3f16116d
ErykDarnowski <github@erykdarnowski.pl>
2022-04-01 13:21:52 +0200

    Add links comment

---
+++ app.js
@@ -0,0 +1,11 @@
+/* LINKS:
+
+@ Tutorial:
+- https://dev.to/code_jedi/web-scraping-in-nodejs-2lkf
+
+@ Stores:
+- https://www.gog.com/pl/game/martha_is_dead
+- https://store.epicgames.com/pl/p/martha-is-dead
+- https://store.steampowered.com/agecheck/app/515960/
+- https://www.xbox.com/pl-pl/games/store/martha-is-dead/9pm6sjbmvqzl
+*/
