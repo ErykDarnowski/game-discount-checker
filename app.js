9f28975687c24bd63bfce0ec4172d66dd10391f8
ErykDarnowski <github@erykdarnowski.pl>
2022-04-01 16:46:33 +0200

    Add prototype scrape implementation

--- app.js
+++ app.js
@@ -9,3 +9,21 @@
 - https://store.steampowered.com/agecheck/app/515960/
 - https://www.xbox.com/pl-pl/games/store/martha-is-dead/9pm6sjbmvqzl
 */
+
+
+const puppeteer = require('puppeteer');
+
+async function scrape() {
+    const browser = await puppeteer.launch({});
+    const page = await browser.newPage();
+
+    await page.goto('https://www.e-licznik.pl');
+    var element = await page.waitForSelector(".tabs > li:nth-child(5)");
+    
+    var text = await page.evaluate(element => element.textContent, element);
+    console.log(text);
+
+    browser.close();
+};
+
+scrape();
