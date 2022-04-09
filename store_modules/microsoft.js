070817e5f3fd297be6f2e7e9b72c94b19f252309
ErykDarnowski <github@erykdarnowski.pl>
2022-04-09 15:22:58 +0200

    Optimize microsoft.js request speed

--- store_modules/microsoft.js
+++ store_modules/microsoft.js
@@ -6,8 +6,41 @@ async function getPriceData(microsoftURL) {
     const page = await browser.newPage();
     
     await page.setDefaultNavigationTimeout(0);
-    await page.goto(microsoftURL); // , {waitUntil: 'networkidle0'}
+    await page.setRequestInterception(true);
     
+    const blockedTypes = [
+        'image',
+        'font',
+        'xhr',
+        'fetch',
+        'stylesheet',
+        'other',
+        'ping'
+    ];
+    const blockedDomains = [
+        'https://mem.gfx.ms/',
+        'https://login.live.com/',
+        'https://account.xbox.com/',
+        'https://www.microsoft.com/',
+        'https://ajax.aspnetcdn.com/',
+        'https://assets.adobedtm.com/',
+        'https://assets-www.xbox.com/',
+        'https://wcpstatic.microsoft.com'
+    ];
+
+    page.on('request', (req) => {
+        const url = req.url();
+        
+        // Blocking types of unneeded requests and some URLSs:
+        if (blockedDomains.some((d) => url.startsWith(d)) || blockedTypes.includes(req.resourceType())) {
+            req.abort();
+        } else {            
+            req.continue();
+        };
+    });
+
+    await page.goto(microsoftURL);
+
     var element = await page.waitForSelector('.Price-module__srOnly___2mBg_');
     var priceEl = await page.evaluate(element => element.innerText.replace("Oryginalna cena: ", "").replace("; cena na wyprzedaży ", ""), element);    
 
