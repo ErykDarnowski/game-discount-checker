e784236e8e75cf616b2b7f80f39d65787e6d087a
ErykDarnowski <github@erykdarnowski.pl>
2022-04-10 18:46:32 +0200

    Implement API instead of puppeteer in microsoft.js

--- store_modules/microsoft.js
+++ store_modules/microsoft.js
@@ -1,67 +1,27 @@
+// microsoft api github: https://github.com/ThomasPe/MS-Store-API
+
 // Imports:
-const { puppeteer, formatPriceToFloat, calculateDiscountPercent } = require('../common.js');
+const { fetch, formatPriceToFloat, calculateDiscountPercent} = require('../common.js');
 
-async function getPriceData(microsoftURL) {
-    const browser = await puppeteer.launch({});
-    const page = await browser.newPage();
-    
-    await page.setCacheEnabled(false);
-    await page.setRequestInterception(true);
-    
-    const blockedTypes = [
-        'image',
-        'font',
-        'xhr',
-        'fetch',
-        'stylesheet',
-        'other',
-        'ping'
-    ];
-    const blockedDomains = [
-        'https://mem.gfx.ms/',
-        'https://login.live.com/',
-        'https://account.xbox.com/',
-        'https://www.microsoft.com/',
-        'https://ajax.aspnetcdn.com/',
-        'https://assets.adobedtm.com/',
-        'https://assets-www.xbox.com/',
-        'https://wcpstatic.microsoft.com'
-    ];
+async function getPriceData(microsoft_URL) {
+    var productId = microsoft_URL.split("?")[0].split("/").pop();
 
-    page.on('request', (req) => {
-        const url = req.url();
-        
-        // Blocking types of unneeded requests and some URLSs:
-        if (blockedDomains.some((d) => url.startsWith(d)) || blockedTypes.includes(req.resourceType())) {
-            req.abort();
-        } else {
-            req.continue();
-        };
-    });
+    // Fetching data from API:
+    return fetch('https://storeedgefd.dsx.mp.microsoft.com/v8.0/sdk/products?market=PL&locale=pl-PL&deviceFamily=Windows.Desktop', {
+        method: "POST",
+        body: `{ "productIds": "${productId}" }`,
+        headers: { "Content-Type": "application/json" }
+    }).then(res => res.json()).then(json => {
+        // Getting general data:
+        var priceData = json["Products"][0]["DisplaySkuAvailabilities"][0]["Availabilities"][0]["OrderManagementData"]["Price"];
 
-    await page.goto(microsoftURL);
-
-    var element = await page.waitForSelector('.Price-module__srOnly___2mBg_');
-    var priceEl = await page.evaluate(element => element.innerText.replace("Oryginalna cena: ", "").replace("; cena na wyprzedaży ", ""), element);    
-
-    browser.close();
-
-    // Check if discount:
-    if (priceEl.split(" zł").length == 2) {
-        var basePrice = formatPriceToFloat(priceEl.split(" zł")[0]);
-
-        //console.log(basePrice + "zł");
-        priceDataArr = [basePrice, basePrice, 0];
-    } else {
-        var basePrice = formatPriceToFloat(priceEl.split(" zł")[0]);
-        var discountPrice = formatPriceToFloat(priceEl.split(" zł")[1]);
+        // Getting specific data:
+        var basePrice = formatPriceToFloat(String(priceData["MSRP"]));
+        var discountPrice = formatPriceToFloat(String(priceData["ListPrice"]));
         var discountPercent = calculateDiscountPercent(basePrice, discountPrice);
-
-        //console.log(basePrice + "zł -> " + discountPrice + "zł = -" + discountPercent + "%");
-        priceDataArr = [basePrice, discountPrice, discountPercent];
-    };    
-
-    return priceDataArr;
+        
+        return [basePrice, discountPrice, discountPercent];
+    });
 };
 
 exports.getPriceData = getPriceData;
