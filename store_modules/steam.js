93967389037bc6c3cfb7a24d665474889f876c80
ErykDarnowski <github@erykdarnowski.pl>
2022-04-17 15:33:20 +0200

    Fix steam.js sometimes returns wrong price (#23)

--- store_modules/steam.js
+++ store_modules/steam.js
@@ -1,10 +1,16 @@
+/*
+- https://steamdb.info/blog/store-prices-api/
+- https://nik-davis.github.io/posts/2019/steam-data-collection/
+- https://stackoverflow.com/questions/29591313/steam-api-grabbing-a-list-of-prices
+*/
+
 // Imports:
 const { fetch, formatPrice } = require('../common.js');
 
 async function getPriceData(steamURL) {
     let splitURL = steamURL.split("app")
     let gameId = splitURL[1].split("/")[1];
-    let apiURL = splitURL[0] + "api/appdetails?appids=" + gameId;
+    let apiURL = splitURL[0] + "api/appdetails?appids=" + gameId + "&cc=PLN";
 
     // Fetching data from API:
     return fetch(apiURL, { method: "GET" }).then(res => res.json()).then((json) => {
